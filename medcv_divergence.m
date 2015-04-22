2801c018506970e6823b2af0b7cabf271676f840
Jacob Panikulam <jpanikul@gmail.com>
2015-04-21 23:28:13 -0400

    Add CDF distance to medcv_divergence

--- medcv_divergence.m
+++ medcv_divergence.m
@@ -15,9 +15,27 @@ function [distance] = divergence(set_1, set_2, method)
 		error(['No such method', method])
 	end
 
+function [dist] = mahalanobis(set_1, set_2)
+	mahal_dist = mahal(set_1', set_2');
+	dist = sum((set_1 - repmat(mean(set_2), 1, length(set_1))).^2, 2);
+end
+
+function [cdf] = compute_cdf(set)
+	cdf = zeros(length(set), 1);
+	for k = 1:length(set)
+		cdf(k) = sum(set(1:k));
+	end
+end
 
 function [dist] = cdf_distance(set_1, set_2)
+	cdf_1 = compute_cdf(set_1);
+	cdf_2 = compute_cdf(set_2);
 
+	dist = 0.0;
+	alpha = 1.8;
+	for k = 1:length(cdf_1)
+		dist = dist + ((cdf_1(k) - cdf_2(k)) ^ alpha)
+	end
 end
 
 function [dist] = kl_divergence(set_1, set_2)
